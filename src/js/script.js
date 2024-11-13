984d6fb56b668732b4f4790c2b55278e45e0e43d
Issabcq <issa.bacq-gadiaga@edu.univ-fcomte.fr>
2024-11-13 11:48:12 +0100

    ouverture/fermeture menu.

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector(".menu")
 const toggle = document.querySelector(".menu-btn")
 toggle.addEventListener("click", (evt) => {
-    nav.ariaHidden = false;
-    toggle.ariaExpanded = true;
+    const isOpen = toggle.ariaExpanded === "true";
+    const isClosed = !isOpen;
+    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
+    nav.ariaHidden = isOpen;
+    toggle.ariaExpanded = isClosed;
 });
