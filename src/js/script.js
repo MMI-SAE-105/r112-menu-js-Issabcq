eaba84fff42241336b6e41482a8a146279543505
Issabcq <issa.bacq-gadiaga@edu.univ-fcomte.fr>
2024-11-13 11:50:43 +0100

    classe noscroll

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,12 @@
-const nav = document.querySelector(".menu")
-const toggle = document.querySelector(".menu-btn")
+const nav = document.querySelector(".menu");
+const toggle = document.querySelector(".menu-btn");
+const body = document.querySelector("body")
+
 toggle.addEventListener("click", (evt) => {
     const isOpen = toggle.ariaExpanded === "true";
     const isClosed = !isOpen;
     console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
     nav.ariaHidden = isOpen;
     toggle.ariaExpanded = isClosed;
+    body.classList.toggle("noscroll", isClosed)
 });
