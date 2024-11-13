5c053702b4665cc745bacbcbdb2a0b6764940fcd
Issabcq <issa.bacq-gadiaga@edu.univ-fcomte.fr>
2024-11-13 11:44:58 +0100

    ouverture menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,5 @@
 const nav = document.querySelector(".menu")
 const toggle = document.querySelector(".menu-btn")
-
 toggle.addEventListener("click", (evt) => {
     nav.ariaHidden = false;
     toggle.ariaExpanded = true;
