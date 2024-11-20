e579258a85425d48cdaecb20769c1fbc268a0d48
Issabcq <issa.bacq-gadiaga@edu.univ-fcomte.fr>
2024-11-20 16:20:30 +0100

    Création de l'arborescence de fichiers

--- src/js/script.js
+++ src/js/script.js
@@ -5,7 +5,6 @@ const body = document.querySelector("body")
 toggle.addEventListener("click", (evt) => {
     const isOpen = toggle.ariaExpanded === "true";
     const isClosed = !isOpen;
-    console.log("isOpen : ", isOpen, "isClosed : ", isClosed);
     nav.ariaHidden = isOpen;
     toggle.ariaExpanded = isClosed;
     body.classList.toggle("noscroll", isClosed)
